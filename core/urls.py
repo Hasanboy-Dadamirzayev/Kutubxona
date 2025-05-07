9bd18d86bf2883e74cac45377a1e807cd3b8a972
Hasanboy-Dadamirzayev <dadamirzayevhasanboy67@gmail.com>
2025-05-08 01:18:05 +0500

    templates fayllar qoshildi

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,23 @@
-"""
-URL configuration for core project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from main.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mualliflar/', mualliflar),
+    path('muallif1/', muallif1),
+    path('kitoblar/', kitoblar),
+    path('kitob/', kitob),
+    path('recordlar/', recordlar),
+    path('tirik', tirik),
+    path('eng_katta/', eng_katta),
+    path('kitobi_kop/', kitobi_kop),
+    path('olinga_sana', olingan_sana),
+    path('tirik_kitob/', tirik_kitob),
+    path('badiiy/', baddiy),
+    path('katta_mualliflar/', katta_mualliflar),
+    path('kitob_soni/', kitob_soni),
+    path('tanlangan_record/', tanlangan_record),
+    path('bitiruvchi_recordlar', bitiruvchi_recordlar),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,110 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from main.models import *
 
-# Create your views here.
+from main.models import *
+
+def mualliflar(request):
+    mualliflar = Muallif.objects.all()
+    context = {
+        'mualliflar': mualliflar
+    }
+    return render(request, 'mualliflar.html', context)
+
+def muallif1(request):
+    muallif = Muallif.objects.get(id=1)
+    context = {
+        'muallif': muallif
+    }
+    return render(request, 'muallif1.html', context)
+
+def kitoblar(request):
+    kitoblar = Kitob.objects.all()
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'kitoblar.html', context)
+
+def kitob(request):
+    kitob = Kitob.objects.get(id=1)
+    context = {
+        'kitob': kitob
+    }
+    return render(request, 'kitob.html', context)
+
+def recordlar(request):
+    recordlar = Record.objects.all()
+    context = {
+        'recordlar': recordlar
+    }
+    return render(request, 'recordlar.html', context)
+
+def tirik(request):
+    tirik_mualliflar = Muallif.objects.filter(tirik=True)
+    context = {
+        'tirik_mualliflar': tirik_mualliflar
+    }
+    return render(request, 'tirik.html', context)
+
+def eng_katta(request):
+    kitoblar = Kitob.objects.order_by('sahifa')[:3]
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'sahifa.html', context)
+
+def kitobi_kop(request):
+    kitobi_kop_mualliflar = Muallif.objects.order_by('kitob_soni')[:3]
+    context = {
+        'kitobi_kop_mualliflar': kitobi_kop_mualliflar
+    }
+    return render(request, 'kitobi_kop_mualliflar.html', context)
+
+def olingan_sana(request):
+    olingan_sana = Record.objects.order_by('olingan_sana')[:3]
+    context = {
+        'olingan_sana': olingan_sana
+    }
+    return render(request, 'olingan_sana.html', context)
+
+def tirik_kitob(request):
+    kitoblar = Kitob.objects.filter(muallif__tirik=True)
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'tirik_kitob.html', context)
+
+def baddiy(request):
+    kitoblar = Kitob.objects.filter(janr='badiiy')
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'badiiy.html', context)
+
+def katta_mualliflar(request):
+    mualliflar = Muallif.objects.order_by('tugilgan_sana')[:3]
+    context = {
+        'mualliflar': mualliflar
+    }
+    return render(request, 'katta_mualliflar.html', context)
+
+def kitob_soni(request):
+    kitoblar = Kitob.objects.filter(muallif__kitob_soni__lt=10)
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'kitob_soni.html', context)
+
+def tanlangan_record(request):
+    record = Record.objects.get(id=1)
+    context = {
+        'record': record
+    }
+    return render(request, 'tanlangan_record.html', context)
+
+def bitiruvchi_recordlar(request):
+    recordlar = Record.objects.filter(talaba__kurs=4)
+    context = {
+        'recordlar': recordlar
+    }
+    return render(request, 'bitiruvchi_recordlar.html', context)
