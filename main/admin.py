24663f3fdb4861c67bd2ca96927b2defe3a373a3
Hasanboy-Dadamirzayev <dadamirzayevhasanboy67@gmail.com>
2025-05-18 22:01:02 +0500

    admin imkoniyatlaridan koplab funksiyalar qoshildi

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Admin)
+
+class KutubxonachiAdmin(admin.ModelAdmin):
+    list_filter = ['ish_vaqti']
+    search_fields = ['ism']
+    search_help_text = "Ism boyicha qidirish"
+
+class MuallifAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ism', 'jins', 'tugilgan_sana', 'kitob_soni', 'tirik')
+    list_display_links = ('id', 'ism')
+    list_editable = ('kitob_soni', 'tirik')
+    search_fields = ['ism']
+    list_filter = ['tirik']
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'talaba', 'kitob', 'admin', 'olingan_sana', 'qaytarish_sana', 'qaytarilgan')
+    list_editable = ['qaytarilgan']
+    list_display_links = ('id', 'talaba')
+    search_fields = ('talaba__ism', 'kitob__nom', 'admin__ism')
+
+
+
+
+admin.site.register(Admin, KutubxonachiAdmin,)
 admin.site.register(Kitob)
 admin.site.register(Talaba)
-admin.site.register(Muallif)
-admin.site.register(Record)
+admin.site.register(Muallif, MuallifAdmin,)
+admin.site.register(Record, RecordAdmin,)
