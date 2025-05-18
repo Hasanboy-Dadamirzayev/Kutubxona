2b5ddb7adfdc45f7d1f2d169f2f2b41874608ac9
Hasanboy-Dadamirzayev <dadamirzayevhasanboy67@gmail.com>
2025-05-18 23:09:13 +0500

    jazzmin qoshildi admin panel imkoniyatlariga

--- main/admin.py
+++ main/admin.py
@@ -7,18 +7,26 @@ class KutubxonachiAdmin(admin.ModelAdmin):
     search_fields = ['ism']
     search_help_text = "Ism boyicha qidirish"
 
+class KitobInline(admin.StackedInline):
+    model = Kitob
+    extra = 1
+
 class MuallifAdmin(admin.ModelAdmin):
     list_display = ('id', 'ism', 'jins', 'tugilgan_sana', 'kitob_soni', 'tirik')
+    inlines = (KitobInline,)
     list_display_links = ('id', 'ism')
     list_editable = ('kitob_soni', 'tirik')
     search_fields = ['ism']
     list_filter = ['tirik']
+
 
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('id', 'talaba', 'kitob', 'admin', 'olingan_sana', 'qaytarish_sana', 'qaytarilgan')
     list_editable = ['qaytarilgan']
     list_display_links = ('id', 'talaba')
     search_fields = ('talaba__ism', 'kitob__nom', 'admin__ism')
+
+
 
 
 
